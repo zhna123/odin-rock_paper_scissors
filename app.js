ca449329f296d241a475f8a4692855bb794beec9
zhna123 <cszhna@gmail.com>
2023-07-24 13:40:37 -0400

    added display section in addition to logs

--- app.js
+++ app.js
@@ -1,69 +1,14 @@
-function computerPlay() {
-    const items = ['Rock', 'Scissors', 'Paper']
-    let index = Math.floor(Math.random() * 10 % items.length)
-    return items[index]
-}
+const playerSelectionDom = document.querySelector(".display > .player");
+const computerSelectionDom = document.querySelector(".display > .computer")
 
-function play(playerSelection, computerSelection) {
-    let player = playerSelection.toLowerCase()
-    let computer = computerSelection.toLowerCase()
-    if (player === "rock") {
-        if (computer === "rock") {
-            console.log("Tie. Both are rock");
-            return 0 
-        } 
-        if (computer === "paper") {
-            console.log("You lost. You are rock. Paper beats rock");
-            return -1 
-        }
-
-        if(computer === "scissors") {
-            console.log("You won. You are rock. Rock beats scissors");
-            return 1 
-        }
-    }
-
-    if(player === "paper") {
-        if (computer === "rock") {
-            console.log("You won. You are paper. Paper beats rock");
-            return 1 
-        } 
-        if (computer === "paper") {
-            console.log("Tie. Both are paper");
-            return 0 
-        }
-
-        if(computer === "scissors") {
-            console.log("You lost. You are paper. Scissors beats paper");
-            return -1 
-        }
-    }
-
-    if(player === "scissors") {
-        if (computer === "rock") {
-            console.log("You lost. You are scissors. Rock beats scissors");
-            return -1 
-        } 
-        if (computer === "paper") {
-            console.log("You won. You are scissors. Scissors beats paper");
-            return 1 
-        }
-
-        if(computer === "scissors") {
-            console.log("Tie. Both are scissors");
-            return 0 
-        }
-    }
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
-const player = document.querySelector("div>#player>span.score")
-const computer = document.querySelector("div>#computer>span.score");
+const player = document.querySelector("div#results>#player>span.score")
+const computer = document.querySelector("div#results>#computer>span.score");
 
 player.textContent=0;
 computer.textContent=0;
+
+let playerScore = 0;
+let computerScore = 0;
 
 const buttons = Array.from(document.querySelectorAll(".btn"));
 buttons.forEach(btn => btn.addEventListener('click', playGame));
@@ -96,6 +41,76 @@ function playGame(e) {
         playAgainBtn.addEventListener('click', function(e) {
             location.reload();
         })
+    }
+}
+
+function computerPlay() {
+    const items = ['Rock', 'Scissors', 'Paper']
+    let index = Math.floor(Math.random() * 10 % items.length)
+    return items[index]
+}
+
+function play(playerSelection, computerSelection) {
+
+    let player = playerSelection.toLowerCase()
+    playerSelectionDom.textContent = playerSelection
+    playerSelectionDom.classList.remove("green")
+    let computer = computerSelection.toLowerCase()
+    computerSelectionDom.textContent = computerSelection.toUpperCase()
+    computerSelectionDom.classList.remove("green")
+    
+    if (player === "rock") {
+        if (computer === "rock") {
+            console.log("Tie. Both are rock");
+            return 0 
+        } 
+        if (computer === "paper") {
+            console.log("You lost. You are rock. Paper beats rock");
+            computerSelectionDom.classList.add("green")
+            return -1 
+        }
+
+        if(computer === "scissors") {
+            console.log("You won. You are rock. Rock beats scissors");
+            playerSelectionDom.classList.add("green")
+            return 1 
+        }
+    }
+
+    if (player === "paper") {
+        if (computer === "rock") {
+            console.log("You won. You are paper. Paper beats rock");
+            playerSelectionDom.classList.add("green")
+            return 1 
+        } 
+        if (computer === "paper") {
+            console.log("Tie. Both are paper");
+            return 0 
+        }
+
+        if(computer === "scissors") {
+            console.log("You lost. You are paper. Scissors beats paper");
+            computerSelectionDom.classList.add("green")
+            return -1 
+        }
+    }
+
+    if (player === "scissors") {
+        if (computer === "rock") {
+            console.log("You lost. You are scissors. Rock beats scissors");
+            computerSelectionDom.classList.add("green")
+            return -1 
+        } 
+        if (computer === "paper") {
+            console.log("You won. You are scissors. Scissors beats paper");
+            playerSelectionDom.classList.add("green")
+            return 1 
+        }
+
+        if(computer === "scissors") {
+            console.log("Tie. Both are scissors");
+            return 0 
+        }
     }
 }
 
